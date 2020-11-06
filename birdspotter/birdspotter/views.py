58662b4535cc14b779d2de824b72887bd433c184
NickKania <nickkania@gmail.com>
2020-11-06 18:00:55 -0500

    Wire up views Note: this may be overkill and some url pages may not be needed

--- birdspotter/birdspotter/views.py
+++ birdspotter/birdspotter/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+
 def index(request):
-	return HttpResponse('This is the accounts index')
+	return HttpResponse('This is the application index')
