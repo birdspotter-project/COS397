59f7eefc4789f544315687e8f68aca7698632197
Nick Kania <nicholas.kania@maine.edu>
2020-11-16 21:53:52 -0500

    Search for shapefile in zip

--- birdspotter/birdspotter/dataio/scripts/import_handler.py
+++ birdspotter/birdspotter/dataio/scripts/import_handler.py
@@ -4,19 +4,24 @@ from fiona.io import ZipMemoryFile
 from birdspotter.dataio.models import Dataset, Shapefile
 from birdspotter.accounts.models import User
 from datetime import datetime
-
+import zipfile
+import io
 
 def import_shapefile(shapefile):
     binary = shapefile.read()
     with ZipMemoryFile(binary) as zip:
-        with zip.open('Damariscove/Damariscov.shp') as f:
+        zf = zipfile.ZipFile(io.BytesIO(binary))
+        file_loc = list(filter(lambda v: v.endswith('.shp'), zf.namelist()))
+        if not file_loc:
+            return 'Error finding shapefile'
+        with zip.open(file_loc[0]) as f:
             shp = gp.GeoDataFrame.from_features(f)
             owner = None
             if settings.DEBUG:
                 owner = User.objects.get_by_natural_key('testUser')
             dataset = Dataset(name='Damaristest', is_public=True, date_created=datetime.now(), owner=owner,
                               raw_data=None, raw_data_id=None)
-            dataset.save()
+            # dataset.save()
             shp_objects = []
             for _, record in shp.iterrows():
                 shp_objects.append(Shapefile(data_set=dataset, island_name=record.IslandName, cireg=record.CIREG,
@@ -27,5 +32,5 @@ def import_shapefile(shapefile):
                                              point_x=record.geometry.x, point_y=record.geometry.y, latitude=record.Lat,
                                              longitude=record.Long, image=None))
             now = datetime.now()
-            Shapefile.objects.bulk_create(shp_objects, 100)
+            # Shapefile.objects.bulk_create(shp_objects, 100)
             return 'Objects created successfully in %d seconds' % (now - datetime.now()).total_seconds()
