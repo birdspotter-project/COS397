2d9324919200ac6607f931dc61e3952ecce151fc
Nick Kania <nicholas.kania@maine.edu>
2020-11-22 23:52:21 -0500

    Remove redundant field

--- birdspotter/birdspotter/accounts/models.py
+++ birdspotter/birdspotter/accounts/models.py
@@ -3,8 +3,6 @@ from django.contrib.auth.models import AbstractUser
 from django.core.validators import validate_comma_separated_integer_list
 
 
-# Create your models here.
-
 class User(AbstractUser):
     username = models.CharField(max_length=30, unique=True)
-    data_sets = models.ManyToManyField('dataio.Dataset')
+    
