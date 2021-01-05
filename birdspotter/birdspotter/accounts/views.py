68aa95ff00f792c6f35bb60fa0948e71025d1291
Nick <nickkania@gmail.com>
2021-01-05 14:49:40 -0500

    Remove LoginForm Use custom loginview Add account dropdown with logout button

--- birdspotter/birdspotter/accounts/views.py
+++ birdspotter/birdspotter/accounts/views.py
@@ -2,19 +2,14 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 
-from .forms import LoginForm
-
-
 def login_view(request):
 	if request.method == 'POST':
 		if user := authenticate(request, username=request.POST['username'], password=request.POST['password']):
 			login(request, user)	
-			print('User %s logged in successfully' % user.username)
-		return redirect('/')
-	else:
-		form = LoginForm()
-		return render('/', {'form': form})
-	return render(request, 'login.html')
+			return redirect('/')
+		else:
+			return redirect('/')
+	return redirect('/')
 
 def logout_view(request):
 	logout(request)
