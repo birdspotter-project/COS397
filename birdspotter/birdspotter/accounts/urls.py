dbf06d5d871c7d48f1146555cb38818c6f1fc5d6
Nick <nickkania@gmail.com>
2021-01-17 20:57:28 -0500

    Account Editing tests remove unused functions

--- birdspotter/birdspotter/accounts/urls.py
+++ birdspotter/birdspotter/accounts/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('profile/', views.account_view),
-	path('login/', views.login_view),
-	path('logout/', views.logout_view)
-]
+    path('profile/', views.account_view),
+    path('login/', views.login_view),
+    path('logout/', views.logout_view)]
