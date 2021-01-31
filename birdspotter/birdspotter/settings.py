1ddb6e48990f7b410c9d93efc7854e9d1affe6d2
Devin Christianson <devin.a.christianson@gmail.com>
2021-01-31 09:53:17 -0500

    clean up majority of known issues

--- birdspotter/birdspotter/settings.py
+++ birdspotter/birdspotter/settings.py
@@ -21,8 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1x0(#3afyyont_8x6yvtog2^&u5gf@9x@&#+o1=onnfg+5e!q^'
+# SECURITY WARNING: keep the secret key used in production secret! 
+# Airtable task is open for this
+SECRET_KEY = '1x0(#3afyyont_8x6yvtog2^&u5gf@9x@&#+o1=onnfg+5e!q^' #nosec
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
