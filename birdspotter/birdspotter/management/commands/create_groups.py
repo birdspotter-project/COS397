4c4f516153197ccf4f8cab7be041127c53fc566f
Nick <nickkania@gmail.com>
2021-02-21 14:37:06 -0500

    Allow for creation of groups via CLI `./manage.py create_groups`

---
+++ birdspotter/birdspotter/management/commands/create_groups.py
@@ -0,0 +1,50 @@
+import logging
+
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import Group
+from django.contrib.auth.models import Permission
+
+PERMISSIONS = {
+    "Admin": {
+        # django permissions
+        "log entry": ["add", "delete", "change", "view"],
+        "group": ["add", "delete", "change", "view"],
+        "permission": ["add", "delete", "change", "view"],
+        "user": ["add", "delete", "change", "view"],
+        "content type": ["add", "delete", "change", "view"],
+        "session": ["add", "delete", "change", "view"],
+
+        # Birdspotter specific permissions
+        "dataset": ["add", "delete", "change", "view", "export"],
+        "shapefile": ["add", "delete", "change", "view"],
+        "image": ["add", "delete", "change", "view"],
+        "rawData": ["add", "delete", "change", "view"],
+    },
+    "Privileged": {
+        "dataset": ["add", "delete", "change", "view", "export"],
+    },
+    "Registered": {
+        "dataset": ["view", "export"],
+    },
+    "Public": {
+        "dataset": ["view"],
+    },
+}
+
+
+class Command(BaseCommand):
+    help = 'Creates groups and permissions for the application'
+    
+    def handle(self, *args, **options):
+        for group in PERMISSIONS:
+            new_group, created = Group.objects.get_or_create(name=group)
+            for model in PERMISSIONS[group]:
+                for permission in PERMISSIONS[group][model]:
+                    dj_str = 'Can {} {}'.format(permission, model)
+                    try:
+                        model_perm = Permission.objects.get(name=dj_str)
+                    except:
+                        logging.warning("Permission %s not found" % dj_str)
+                        continue
+                    print("Adding %s" % model_perm)
+                    new_group.permissions.add(model_perm)
