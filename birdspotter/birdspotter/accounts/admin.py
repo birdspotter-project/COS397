b4be429b2f357a9c64c2e64f3073d867f1a5169f
Nick <nickkania@gmail.com>
2021-02-25 23:30:43 -0500

    Model admin

--- birdspotter/birdspotter/accounts/admin.py
+++ birdspotter/birdspotter/accounts/admin.py
@@ -1,9 +1,15 @@
 from django.contrib.auth.admin import UserAdmin as usradmin
-from django.contrib import admin
-from birdspotter.accounts.models import User
+from django.contrib import admin as admin
+
+from birdspotter.accounts.models import User, GroupRequest
 
 class UserAdmin(usradmin):
 	pass
 
 
 admin.site.register(User, UserAdmin)
+
+class GroupRequestAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(GroupRequest, GroupRequestAdmin)
