7cf0bc266963d355daa6a3befed20c88b21d2128
Devin Christianson <38331414+devinchristianson@users.noreply.github.com>
2021-03-05 16:15:41 -0500

    Upload via NGINX for higher-performance and chunking (#36)
    * initial nginx
    
    * initial nginx
    
    * working uploads via nginx
    
    * server-side upload changes, docker updates
    
    * correct reviewer comments

--- birdspotter/birdspotter/views.py
+++ birdspotter/birdspotter/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from birdspotter.dataio.scripts.get_user_datasets import get_datasets_for_user
 from birdspotter.accounts.models import User
+from django.http import HttpResponse
 
 
 def index(request):
@@ -18,3 +19,32 @@ def index(request):
     else:
         datasets = None
     return render(request, 'index.html', {'datasets': datasets})
+
+
+# How to correctly send a fully compliant HTTP 204 response, based on https://code.djangoproject.com/ticket/16632
+class HttpResponseNoContent(HttpResponse):
+    """Special HTTP response with no content, just headers.
+
+    The content operations are ignored.
+    """
+
+    def __init__(self, content="", mimetype=None, status=None, content_type=None): # noqa
+        super().__init__(status=204)
+
+        if "content-type" in self._headers:
+            del self._headers["content-type"]
+
+    def _set_content(self, value):
+        pass
+
+    def _get_content(self, value):
+        pass
+
+
+def auth(request):
+    """
+    Used by NGINX to check if the user is authorized with minimal overhead.
+    """
+    if request.user.is_authenticated:
+        return HttpResponseNoContent()
+    return HttpResponse(status=403)
