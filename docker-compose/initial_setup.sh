0f7591d50ca377be8f256ccb7b8bb6f89b2f9a48
Devin Christianson <devin.a.christianson@gmail.com>
2021-03-08 14:25:53 -0500

    remove slurm from initial_setup.sh

--- docker-compose/initial_setup.sh
+++ docker-compose/initial_setup.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-docker-compose exec -d slurmctld /usr/bin/sacctmgr --immediate add cluster name=linux
-docker-compose restart slurmdbd slurmctld
-
 docker-compose exec -d birdspotter python3 manage.py makemigrations
 docker-compose exec -d birdspotter python3 manage.py migrate
 docker-compose exec -d birdspotter python3 manage.py createsuperuser --noinput
