1d0b0e0bb3a9cd203f5aabe7a2b2be7f45e9e3f9
Devin Christianson <38331414+devinchristianson@users.noreply.github.com>
2021-03-12 10:07:01 -0500

    Fix missing whitespace

--- birdspotter/birdspotter/analysis/scripts/update_job.py
+++ birdspotter/birdspotter/analysis/scripts/update_job.py
@@ -4,6 +4,8 @@ from django.conf import settings
 from birdspotter.dataio.scripts.import_handler import import_data
 import datetime
 from birdspotter.dataio.models import Dataset
+
+
 def handle_update(message):
     print(message.subject)
     update = parse_slurm(message.subject)
@@ -13,6 +15,8 @@ def handle_update(message):
     if job.status == "COMPLETED":
         import_results(job.external_job_id, job.owner)
         print("COMPLETED job " + job.external_job_id)
+        
+        
 def parse_slurm(subj):
     raw = re.findall(r"Slurm Job_id=(\d*) Name=([\w\d]*) (\w*), Run time ([\d:]*), (\w*), ExitCode (\d*)", subj)
     d = dict()
@@ -21,6 +25,8 @@ def parse_slurm(subj):
     d['runtime'] = raw.group(4)
     d['status'] = raw.group(5)
     return d
+
+
 def import_results(job_id, user):
     analysisJob = AnalysisJob.objects.get(external_job_id=job_id)
     dataset = Dataset.objects.get(dataset_id=analysisJob.dataset_id)
