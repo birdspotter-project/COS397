89aaeb311eac36caf48dac7d19246cbeb3a6280f
Nick <nickkania@gmail.com>
2021-03-19 12:12:20 -0400

    Remove unused import

--- birdspotter/birdspotter/dataio/views.py
+++ birdspotter/birdspotter/dataio/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
