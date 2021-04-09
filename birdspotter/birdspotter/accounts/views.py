82de462d33beb9fc526dcb39957034fe930a8156
Nick Kania <nickkania@gmail.com>
2021-04-09 16:27:34 -0400

    unused import

--- birdspotter/birdspotter/accounts/views.py
+++ birdspotter/birdspotter/accounts/views.py
@@ -2,7 +2,6 @@ from django.contrib import messages
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from django.core.mail import send_mail
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
