5c19fbcbf47b99f5995ba007b6fa0fc8c744c965
Nick Kania <nickkania@gmail.com>
2021-04-09 14:04:27 -0400

    Remove permissions from user admin page, make last_login & date_joined view only

--- birdspotter/birdspotter/accounts/admin.py
+++ birdspotter/birdspotter/accounts/admin.py
@@ -4,9 +4,16 @@ from django.contrib.auth.admin import UserAdmin
 from birdspotter.accounts.models import User, GroupRequest
 
 
-class UserModelAdmin(UserAdmin):
-    pass
 
+class UserModelAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups',)}),
+        (('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    readonly_fields = ('last_login', 'date_joined')
 
 admin.site.register(User, UserModelAdmin)
 admin.site.register(GroupRequest)
